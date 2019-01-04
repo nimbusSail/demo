1ac650f797f25c3b953e3e88bd7e577247f5323b
lyf <nimbus_sail@163.com>
2019-01-04 14:59:41 +0800

    web security

---
+++ src/main/java/com/lyf/demo/Controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.lyf.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+//    @RequestMapping("/hi")
+//    public String hi() {
+//        return "hi";
+//    }
+//
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "hello";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+}
